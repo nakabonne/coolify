97b0e606b24661269de1b3a033e839e3657b84af
ryonakao <rodriguez.nak@gmail.com>
2017-09-05 11:26:35 +0900

    Divide processing between vowels and others.

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "math/rand"
+import (
+	"bufio"
+	"math/rand"
+	"os"
+	"time"
+)
 
 const (
 	duplicateVowel bool = true
@@ -8,4 +13,22 @@ const (
 
 func randBool() bool {
 	return rand.Intn(2) == 0
+}
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		word := []byte(s.Text())
+		if randBool() {
+			var vI int = -1
+			for i, char := range word {
+				switch char {
+				case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+					if randBool() {
+						vI = i
+					}
+				}
+			}
+		}
+	}
 }
